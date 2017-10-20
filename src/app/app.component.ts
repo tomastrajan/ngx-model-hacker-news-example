8f325c5677a7c24f3c7c74ee5fdbb3b2f54f3a46
tomastrajan <tomas.trajan@gmail.com>
2017-10-21 00:01:29 +0800

    feat(env): add env path alias, api url and env names

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { environment as env } from '@env/environment';
+
 @Component({
   selector: 'nmhne-root',
   templateUrl: './app.component.html',
