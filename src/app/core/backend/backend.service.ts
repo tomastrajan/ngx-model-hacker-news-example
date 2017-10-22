1b780280039ca5bb2f4f8cf54bc15cf6600375c1
tomastrajan <tomas.trajan@gmail.com>
2017-10-22 12:12:14 +0800

    feat(core): add backend get method, export backend service, import ngx-model

--- src/app/core/backend/backend.service.ts
+++ src/app/core/backend/backend.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { environment } from '@env/environment';
+
 @Injectable()
 export class BackendService {
   constructor(private http: HttpClient) {}
+
+  get(url: string) {
+    return this.http.get(`${environment.api.url}${url}`);
+  }
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { NgxModelModule } from 'ngx-model';
 
 import { BackendService } from './backend/backend.service';
 
 @NgModule({
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, NgxModelModule],
   declarations: [],
   providers: [BackendService]
 })
