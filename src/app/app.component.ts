d09228cd800777bc48aae5211e47cb61cd263cfe
tomastrajan <tomas.trajan@gmail.com>
2017-10-22 12:19:09 +0800

    feat(title): show title based on route data if available

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import 'rxjs/add/operator/filter';
 
 import { environment as env } from '@env/environment';
 
@@ -7,6 +10,23 @@ import { environment as env } from '@env/environment';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'nmhne';
+
+  constructor(private router: Router, private titleService: Title) {}
+
+  ngOnInit(): void {
+    this.router.events
+      .filter(event => event instanceof ActivationEnd)
+      .subscribe((event: ActivationEnd) => {
+        let lastChild = event.snapshot;
+        while (lastChild.children.length) {
+          lastChild = lastChild.children[0];
+        }
+        const { title } = lastChild.data;
+        this.titleService.setTitle(
+          title ? `${title} - ${env.appName}` : env.appName
+        );
+      });
+  }
 }
