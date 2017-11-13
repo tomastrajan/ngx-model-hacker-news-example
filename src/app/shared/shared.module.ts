402728d9768de940513966612632015292612914
tomastrajan <tomas.trajan@gmail.com>
2017-11-13 12:33:09 +1000

    feat(shared): add material icons capabilities

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,23 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [CommonModule, MatToolbarModule, MatCardModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   declarations: [],
-  exports: [CommonModule, MatToolbarModule, MatCardModule, MatButtonModule]
+  exports: [
+    CommonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
+  ]
 })
 export class SharedModule {}
